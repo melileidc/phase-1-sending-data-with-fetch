89c26011a1ad07d34f10799af32ecdad6e2068e8
melilei <melilei6784@gmail.com>
2024-07-14 14:30:29 +0300

    new file

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
 // Add your code here
+const dogUrl=`http://localhost:3000/dogs`
+const catsUrl=`http://localhost:3000/cats`
+const usersUrl=`http://localhost:3000/users`
+const robotsUrl=`http://localhost:3000/robots`
+const name=document.getElementById('name')
+// let form=document.querySelector('form').addEventListener('submit',(e)=>{
+//     e.preventDefault()
+
+// })
+// const formData = {
+//     name:e.target.name.value,
+//     email:e.target.email.value,
+//   };
+  
+  // method: "POST" is missing from the object below
+  const configurationObject = {
+    method:'POST',
+    headers: {
+    "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(formData),
+  };
+  
+  //POST USERS FORM DATA
+  const usersFormData = {
+    name:"Steve",
+    email:"steve@steve.com"
+  };
+  
+
+
+    function submitData(name){
+        return fetch(`http://localhost:3000/users/${name}`, configurationObject)
+        .then(response=>response.json())
+        .then(users=>users)
+
+        .catch( (error)=>{
+          alert("Bad things! Ragnarők!");
+          console.log(error.message);
+        });
+    
+    }
